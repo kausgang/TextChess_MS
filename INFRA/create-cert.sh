4733807564319075cda4da7af6f9651f06bf5bb9
Kaustav Ganguli <write.to.kaustav@gmail.com>
2021-10-01 14:25:11 -0400

    Update create-cert.sh

--- INFRA/create-cert.sh
+++ INFRA/create-cert.sh
@@ -1,2 +1,3 @@
 # NGINX certificate create
+rm ../CLIENT/cert/nginx.*
 openssl req -x509 -config config.conf -nodes -days 365 -newkey rsa:2048 -keyout ../CLIENT/cert/nginx.key -out ../CLIENT/cert/nginx.crt
