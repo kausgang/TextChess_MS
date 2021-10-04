8316010753e0d2923f4294d924d9668af2d315fa
Kaustav Ganguli <write.to.kaustav@gmail.com>
2021-10-03 23:25:10 -0400

    ditched docker-compose

--- ENGINE/CONFIG/config.js
+++ ENGINE/CONFIG/config.js
@@ -1,6 +1,6 @@
 config = {};
 
-config.rabbitMqServer = "amqp://rabbitmq:5672";
+config.rabbitMqServer = "amqp://192.168.1.214:5672";
 config.receiverQueueName = "user-engine";
 config.senderQueueName = "engine-user";
 
